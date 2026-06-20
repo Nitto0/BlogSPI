727b30f901fff3e9ed99de1a1d0b23bdbdbd3016
Makar <makarsidorevic@gmail.com>
2026-06-20 22:37:21 +0300

    New update

--- src/app.py
+++ src/app.py
@@ -1,5 +1,15 @@
 from flask import Flask
 from config import Config
+from src.models.database import db
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+db.init_app(app)
+migrate = Migrate(app, db)
+
+
+@app.before_request
+def create_table():
+    db.create_all()
--- src/models/user.py
+++ src/models/user.py
@@ -1,11 +1,23 @@
 from src.models.database import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model):
+    __tablename__ = "User"
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
+
     email = db.Column(db.String(120), nullable=False, unique=True)
     password_hash = db.Column(db.String(128), nullable=False)
+
     created_at = db.Column(db.DateTime, default=db.func.now())
     is_active = db.Column(db.Boolean, default=True)
+
     posts = db.relationship('Post', backref='author', lazy=True)
+
+    def set_password(self, password: str):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password: str) -> bool:
+        return check_password_hash(self.password_hash, password)
